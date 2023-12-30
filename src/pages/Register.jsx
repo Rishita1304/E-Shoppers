c55480a762ba7c72f68344cb3268f94ce8a37940
Rishita Srivastava <rixsri.13@gmail.com>
2023-12-30 19:20:11 +0530

    new

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -91,7 +91,6 @@ const Register = () => {
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          </Link>
           <Button onClick={handleSubmit}>CREATE</Button>
         </Form>
       </Wrapper>
